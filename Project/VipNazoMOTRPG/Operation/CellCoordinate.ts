65c6b129309f09cbf4e3dabc0d5eeb1e651f2579
fraipengin <fraipengin@gmail.com>
2015-11-03 23:27:19 +0900

    operation画面の高さの位置変更

--- Project/VipNazoMOTRPG/Operation/CellCoordinate.ts
+++ Project/VipNazoMOTRPG/Operation/CellCoordinate.ts
@@ -1,13 +1,13 @@
 ﻿/// <reference path="../Util/Rect.ts" />
 
-module field {
+module operation {
 
     /** セルのキャンバス上の座標の計算 */
     export class CellCoordinate {
 
         static toRect(x: number, y: number): Rect {
             var kWidth: number = 100;
-            var kHeight: number = 100;
+            var kHeight: number = 600;
             var kMargin: number = 25;
             var kPadding: number = 10;
 
